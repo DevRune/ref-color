b42d71d59a3524ead7d94458d4852a64b6adba69
DevRune <98558431+DevRune@users.noreply.github.com>
2026-01-28 16:02:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -208,15 +208,12 @@ function bepaalKleur() {
 
   const gekozen = kiesUit[Math.floor(Math.random() * kiesUit.length)];
   let alleOpties = kiesUit.map(c => c.name).join(", ");
-  let reden = perfect.length > 0 ? "geen soft conflict" : "soft conflict aanwezig";
 
   advies.innerHTML = `
     ✅ <b>Advies:</b> ${gekozen.name} <br>
     ℹ️ Beschikbare opties: ${alleOpties} <br>
-    ℹ️ Reden: ${reden}
-    ℹ️ Soft: ${soft}
-    ℹ️ Hard: ${hard}
   `;
 }
+
 
 
